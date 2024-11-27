e9a3421cd695a3c303da1ea7b4a7ee8e69271b57
locktum1 <locktum123@gmail.com>
2024-11-27 17:04:18 +0000

    Fixed issues with serving

---
+++ .history/routes/web_20241125165056.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LogInController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\PaymentController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\PayPalController;
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('catalogue');
+});
+
+//Auth routes
+Route::get('/login', [LogInController::class, 'LogInView'])->name('login');
+Route::post('/submit-login', [LogInController::class, 'Submit'])->name('submit.login');
+
+Route::get('/register', [RegisterController::class, 'RegisterView'])->name('register');
+Route::post('/submit-register', [RegisterController::class, 'Submit'])->name('submit.register');
+
+Route::post('/logout', function(){
+    Auth::logout();
+    return redirect()->back();
+})->name('logout');
+
+//Subscription routes
+Route::get('/subscribe', [SubscriptionController::class, 'SubscribeView'])->name('subscribe');
+
+Route::get('auth/paypal/redirect', [PayPalController::class, 'redirectToProvider']);
+Route::get('auth/paypal/callback', [PayPalController::class, 'handleProviderCallback']);
