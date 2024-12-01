d507028244be1a388ceb72acdc87f61e9d43833a
locktum1 <locktum123@gmail.com>
2024-12-01 18:48:50 +0000

    Completed review process

---
+++ .history/routes/web_20241201174606.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\MediaController;
+use App\Http\Controllers\LogInController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\PaymentController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\PayPalController;
+use App\Http\Controllers\ReviewController;
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('catalogue');
+});
+
+Route::get('/media', [MediaController::class, 'MediaView'])->name('media');
+Route:post('/submit-review', [MediaController::class])
+
+//Auth routes
+Route::get('/login', [LogInController::class, 'LogInView'])->name('login');
+Route::post('/submit-login', [LogInController::class, 'Submit'])->name('submit.login');
+
+Route::get('/register', [RegisterController::class, 'RegisterView'])->name('register');
+Route::post('/submit-register', [RegisterController::class, 'Submit'])->name('submit.register');
+
+Route::post('/logout', function(){
+    Auth::logout();
+    return redirect()->back();
+})->name('logout');
+
+//Subscription routes
+Route::get('/subscribe', [SubscriptionController::class, 'SubscribeView'])->name('subscribe');
+
+Route::get('auth/paypal/redirect', [PayPalController::class, 'redirectToProvider']);
